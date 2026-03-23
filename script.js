aa277782600e1521cb233683e0dfa63a608b06e6
miojo01 <bats.leco@gmail.com>
2026-03-23 19:43:53 -0300

    second commit

--- script.js
+++ script.js
@@ -12,6 +12,27 @@ let gameBoard = [
     '', '', '',
 ];
 
+const winningCombinations = [
+    [0, 1, 2],
+    [0, 4, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8],
+];
+
+function checkWinner() {
+    winningCombinations.forEach((combination) => {
+        const [a, b, c] = combination;
+
+        if (gameBoard[a] === gameBoard[b] && gameBoard[a] === gameBoard[c]) {
+            console.log(`Jogador ${currentPlayer} venceu!`);
+        }
+    });
+};
+
 for (let i = 0; i < 9; i++) {
     const cell = document.createElement('div');
     cell.classList.add('cell');
@@ -19,6 +40,15 @@ for (let i = 0; i < 9; i++) {
 
     cell.addEventListener('click', () => {
         if (gameOver) return;
+        if (gameBoard[i] !== '') return;
+
+        gameBoard[i] = currentPlayer;
+
+        cell.innerText = currentPlayer;
+        
+        currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+
+        document.getElementById('turno').innerText = `Vez do Jogador ${currentPlayer}`;
     });
 }
 
